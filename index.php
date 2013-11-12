a5a6a8fc07df804227cee017e0369b10916d4d24
Jordan Morano <jordan.morano@gmail.com>
2013-11-12 11:38:48 -0500

    favicon url fix

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Morano Gelato - Puro Gelato Italiano</title>
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="shortcut icon" href="/favicon.ico" >
+	<link rel="shortcut icon" href="img/favicon.ico" >
 </head>
 <body>
 	<div id="wrapper">
