08a91ac5740aac5c11a0bf8831e80ef269e38f8f
Jordan Morano <jordan.morano@gmail.com>
2013-11-13 18:40:21 -0500

    w3c compliant

--- index.php
+++ index.php
@@ -1,73 +1,78 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Morano Gelato - Puro Gelato Italiano</title>
-	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	
-	<!-- favicons -->
-	<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-	<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160" />
-	<meta name="msapplication-TileColor" content="#da532c" />
-	<meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-	<meta name="msapplication-square70x70logo" content="/mstile-70x70.png" />
-	<meta name="msapplication-square150x150logo" content="/mstile-150x150.png" />
-	<meta name="msapplication-wide310x150logo" content="/mstile-310x150.png" />
-</head>
-<body>
-	<div id="wrapper">
-		<header id="top">
-			<img id="top_img" src="img/logo.jpg">
-			<nav>
-				<a id="about">About</a>
-				<a id="press">Press</a>
-				<a id="contact">Contact</a>
-			</nav>
-		</header>
-		<div id="slide_parent">
-			<div id="slideshow">
-				<div id="img_left"></div>
-				<div id="img_right"></div>
+	<head>
+		<meta charset="utf-8"> 
+
+		<title>Morano Gelato - Pure Italian Gelato</title>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
+		<!-- favicons -->
+		<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png" />
+		<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160" />
+		<meta name="msapplication-TileColor" content="#da532c" />
+		<meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+		<meta name="msapplication-square70x70logo" content="/mstile-70x70.png" />
+		<meta name="msapplication-square150x150logo" content="/mstile-150x150.png" />
+		<meta name="msapplication-wide310x150logo" content="/mstile-310x150.png" />
+
+		<!-- open graph-->
+		<meta property="og:image" content="http://moranogelato.com/img/site.jpg" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<header id="top">
+				<img id="top_img" src="img/logo.jpg" alt="Morano Gelato Inc Logo">
+				<nav>
+					<a id="about">About</a>
+					<a id="press">Press</a>
+					<a id="contact">Contact</a>
+				</nav>
+			</header>
+			<div id="slide_parent">
+				<div id="slideshow">
+					<div id="img_left"></div>
+					<div id="img_right"></div>
+				</div>
 			</div>
+			<section id="content">
+				<article>
+					<header>
+						<h2>Pure Italian Gelato</h2>
+					</header>
+					<p>
+						Sicilian Style Gelato<br>
+						Fresh &amp; Local Ingredients<br>
+						Artisanal Product Made Daily On-Site<br>
+					</p>
+
+					<p class="announcement">Check out our Hanover location!</p>
+					<a class="big_link" href="http://www.moranogelatohanover.com">Morano Gelato Hanover</a>
+				</article>
+			</section>
+			<footer>
+				<p>&copy; <?php echo date('Y');?> Morano Gelato Inc.</p>
+				<p>PO Box 5647 Hanover, NH 03755 :: 347.210.4933 :: moranogelato@gmail.com</p>
+				<nav>
+					<a href="https://www.facebook.com/moranogelatoinc?ref=br_tf">FB</a>
+					<a href="https://twitter.com/MoranoGelato">Twitter</a>
+					<a href="http://instagram.com/">Instagram</a> <!-- need to fix this link-->
+				</nav>
+				<p><a id="self-credit" href="http://jordanmorano.biz">A Jordan Paul Morano Design Project</a></p>
+			</footer>
 		</div>
-		<section id="content">
-			<article>
-				<header>
-					<h2>Pure Italian Gelato</h2>
-				</header>
-				<p>
-					Sicilian Style Gelato<br>
-					Fresh &amp; Local Ingredients<br>
-					Artisanal Product Made Daily On-Site<br>
-				</p>
 
-				<p class="announcement">Check out our Hanover location!</p>
-				<a class="big_link" href="http://www.moranogelatohanover.com">Morano Gelato Hanover</a>
-			</article>
-		</section>
-		<footer>
-			<p>&copy; <?php echo date('Y');?> Morano Gelato Inc.</p>
-			<p>PO Box 5647 Hanover, NH 03755 :: 347.210.4933 :: moranogelato@gmail.com</p>
-			<nav>
-				<a href="https://www.facebook.com/moranogelatoinc?ref=br_tf">FB</a>
-				<a href="https://twitter.com/MoranoGelato">Twitter</a>
-				<a href="http://instagram.com/">Instagram</a> <!-- need to fix this link-->
-			</nav>
-			<p><a id="self-credit" href="http://jordanmorano.biz">A Jordan Paul Morano Design Project</a></p>
-		</footer>
-	</div>
-
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script type="text/javascript" src="js/script.js"></script>
-</body>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</body>
 </html>
