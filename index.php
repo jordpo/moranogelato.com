3dc139a5bb92a9569f7c9aa19a9f51c1344a7b7c
Jordan Morano <jordan.morano@gmail.com>
2013-11-16 20:54:51 -0500

    fix

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
 		</div>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script type="text/javascript" src="js/script.js"></script>
 		<script type="text/javascript" src="js/validEmail.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
 	</body>
 </html>
