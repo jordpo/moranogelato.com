164327514cae1c9d3fff7eaf018a2cfa209a1914
Jordan Morano <jordan.morano@gmail.com>
2014-03-22 17:36:08 -0400

    Add meta tag for responsiveness

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 		<!-- Stylesheet -->
 		<link rel="stylesheet" type="text/css" href="css/style.css?version=6">
 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- favicons -->
 		<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-touch-icon-57x57.png" />
 		<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-touch-icon-114x114.png" />
